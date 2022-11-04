7c80c291f044b07b1e77bbb428a229ead5f8e4e4
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-04 10:40:44 +0100

    🚑 Use old js code

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-import { join } from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 const nextConfig = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [join(__dirname, 'styles')],
-    },
-    swcMinify: true,
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
