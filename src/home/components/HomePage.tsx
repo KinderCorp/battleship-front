7303d5ff25453d970a9143237778e0f954cfd27f
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-06 20:14:00 +0100

    🚀 Example

--- src/home/components/HomePage.tsx
+++ src/home/components/HomePage.tsx
@@ -1,10 +1,25 @@
+import { decrement, increment } from '@home/reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectHomeValue } from '@home/selectors';
+
 /**
  * Home page component.
  *
  * @return {JSX.Element}
  */
 export const HomePage = (): JSX.Element => {
-  return <p>HomePage</p>;
+  const value = useSelector(selectHomeValue);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h1>HomePage</h1>
+      <hr />
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <p>{value}</p>
+    </>
+  );
 };
 
 export default HomePage;
