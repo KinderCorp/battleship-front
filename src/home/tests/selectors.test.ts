b666d032055cadb78a157c54bce801734855d169
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-06 21:53:14 +0100

    🎨 Rename variable

--- src/home/tests/selectors.test.ts
+++ src/home/tests/selectors.test.ts
@@ -1,13 +1,13 @@
 import * as homeConstants from '@home/constants';
 import * as homeSelectors from '@home/selectors';
-import homeState from '@mocks/state/homeStateMock';
+import homeStateMock from '@mocks/state/homeStateMock';
 
 const { NAME } = homeConstants;
 
 describe('home/selectors', () => {
   it('should return value', () => {
-    const selected = homeSelectors.selectHomeValue(homeState);
-    const expectedResult = homeState[NAME].value;
+    const selected = homeSelectors.selectHomeValue(homeStateMock);
+    const expectedResult = homeStateMock[NAME].value;
 
     expect(selected).toEqual(expectedResult);
   });
