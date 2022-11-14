804fbb50257dfe3a35fccff2ee1b65c551358485
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-14 22:43:21 +0100

    🎨 Fix some code

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,18 +1,4 @@
 import '@testing-library/jest-dom';
-/**
- * Fake match media.
- *
- * @return {object}
- */
-const fakeMatchMedia = () => ({
-  addListener: jest.fn(),
-  matches: false,
-  removeListener: jest.fn(),
-});
-
-window.matchMedia = window.matchMedia || fakeMatchMedia;
-
-window.open = jest.fn();
 
 jest.mock('next/config', () => () => ({
   publicRuntimeConfig: {
--- src/api/constants.ts
+++ src/api/constants.ts
@@ -9,6 +9,7 @@ export const HTTP_STATUS_CODE: Record<string, number> = {
   CREATED: 201,
   INTERNAL_SERVER_ERROR: 500,
   NO_CONTENT: 204,
+  NOT_FOUND: 404,
   OK: 200,
   UNAUTHORIZED: 401,
 };
