44628c9e3b4bbd734bdff1bece63b4a1cdcbf37b
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-18 13:29:13 +0100

    🚀 StringHelpers

---
+++ src/shared/_helpers/StringHelpers.ts
@@ -0,0 +1,23 @@
+class StringHelpers {
+  /**
+   * Check if the value is a string.
+   *
+   * @param {any} value Value to check
+   * @return {boolean}
+   */
+  static isString(value: any): boolean {
+    return typeof value === 'string';
+  }
+
+  /**
+   * Check if the value is empty.
+   *
+   * @param {any} value Value to check
+   * @return {boolean}
+   */
+  static isEmpty(value: any): boolean {
+    return !(this.isString(value) && value.trim().length > 0);
+  }
+}
+
+export default StringHelpers;
