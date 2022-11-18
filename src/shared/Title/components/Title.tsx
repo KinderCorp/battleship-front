1b5d52ee3662c05bcdbbbddfbbe04ed50f2ccd83
manonfrgs <manon.fargues@ynov.Com>
2022-11-18 11:04:03 +0100

    :rocket: Add Icon in TitleComponent

--- src/shared/Title/components/Title.tsx
+++ src/shared/Title/components/Title.tsx
@@ -1,6 +1,7 @@
 // import de l'Icon
 // import classNames from 'clsx';
 // import { useMemo } from 'react';
+import Icon from '@shared/Icon/components/Icon';
 
 
 /**
@@ -13,6 +14,7 @@ export interface Props {
     title: string,
     subtitle?: string,
     children: React.ReactNode;
+    iconName: null,
     className?: '';
     type : string,
     // iconName: Icon, // Models icon, sera défini lorsque la PR sur les composants icones sera faites 
@@ -36,7 +38,11 @@ export const Title = ({ type, title, subtitle, className }: Props): JSX.Element 
 
   return (
     <>
-      {/* <TitleComponent className={titleclassName}>{title}</TitleComponent> */}
+      {/* <TitleComponent className={titleclassName}>
+      {iconName && (
+        <Icon borderColor={iconStyle.borderColor} color={iconStyle.color} name={iconName} />
+      )}
+      {title}</TitleComponent> */}
       {subtitle && <p>{subtitle}</p>}
     </>
   );
