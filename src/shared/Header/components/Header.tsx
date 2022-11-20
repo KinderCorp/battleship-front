f325ee9c40176bd1c4ab7a03d75edcd9058e9ea2
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-20 18:33:15 +0100

    🚧 Header

--- src/shared/Header/components/Header.tsx
+++ src/shared/Header/components/Header.tsx
@@ -2,6 +2,7 @@ import classNames from 'clsx';
 import { useMemo } from 'react';
 
 import type { HeaderProps } from '@shared/Header/models';
+import Title from '@shared/Title/components/Title';
 
 /**
  * Header component.
@@ -9,10 +10,25 @@ import type { HeaderProps } from '@shared/Header/models';
  * @param {HeaderProps} props Props
  * @return {JSX.Element}
  */
-const Button = ({ className = '' }: HeaderProps): JSX.Element => {
-  const headerClassName = useMemo((): string => classNames('header', className), [className]);
+const Header = ({
+  className = '',
+  height = 'medium',
+  leftSide = null,
+  rightSide = null,
+  title,
+}: HeaderProps): JSX.Element => {
+  const headerClassName = useMemo(
+    (): string => classNames('header', height, className),
+    [className, height],
+  );
 
-  return <div className={headerClassName}></div>;
+  return (
+    <div className={headerClassName}>
+      {leftSide && <div className="header-left-side">{leftSide}</div>}
+      <Title className="header-title" title={title} type="h2" />
+      {rightSide && <div className="header-right-side">{rightSide}</div>}
+    </div>
+  );
 };
 
-export default Button;
+export default Header;
