61b2d7d12ce98240618fe6cb6c732bf41c1823bd
manonfrgs <manon.fargues@ynov.Com>
2022-11-24 12:23:24 +0100

    :rocket: Completed BlockContainer

--- src/shared/BlockContainer/components/BlockContainer.tsx
+++ src/shared/BlockContainer/components/BlockContainer.tsx
@@ -1,11 +1,8 @@
-import type { BlockContainerProps } from '@shared/BlockContainer/model';
-import classNames from 'clsx';
-import { COLORS } from '@core/constants';
 import { useMemo } from 'react';
 
-// import type { ButtonProps } from '@shared/Button/models';
-import Icon from '@shared/Icon/components/Icon';
-// import Button from '@shared/Button/components/Button';
+import type { BlockContainerProps } from '@shared/BlockContainer/model';
+import classNames from 'clsx';
+import Title from '@shared/Title/components/Title';
 
 /**
  * BlockContainer component.
@@ -13,8 +10,8 @@ import Icon from '@shared/Icon/components/Icon';
  * @param {BlockContainerProps} props Props
  * @return {JSX.Element}
  */
-export const BlockContainer = ({
-    children = '',
+const BlockContainer = ({
+    children,
     className = '',
     iconName = null,
     title = '',
@@ -30,16 +27,12 @@ export const BlockContainer = ({
 
     return (
         <>
-          <BlockContainer  className={blockContainerClassName} title={title} iconName="Share">
-            {iconName && (
-              <Icon borderColor={COLORS.TRANSPARENT} color={COLORS.PURPLE_LIGHT} name={iconName} />
-            )}
-            {/* <TextContainer value="https://www.battleship.fr/join?code=8b1a292b05c946bc945b8d14a105dd5d" /> */}
-            {/* <Button onClick={} iconName="Copy" /> */}
-          </BlockContainer>
+          <div className={blockContainerClassName}>
+            <Title className="block-container-title" title={title} type="h2" iconName={iconName} />
+            <div className={blockContainerClassName}>{children}</div>
+          </div>
         </>
     );
 };
-
 
 export default BlockContainer;
