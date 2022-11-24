9bcdc2399d6662cc2014397c3f046c4c4d5023dd
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-24 21:12:10 +0100

    💄 Styles

--- src/shared/TextContainer/models.ts
+++ src/shared/TextContainer/models.ts
@@ -1,6 +1,9 @@
 import type { ReactNode } from 'react';
 
+export type TextContainerHeight = 'medium' | 'small';
+
 export interface TextContainerProps {
   children: ReactNode;
   className?: string;
+  height?: TextContainerHeight;
 }
