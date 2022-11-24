829ba80d9780fa5fb3c5884b680cbfdfc1bc4182
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-11-24 16:35:37 +0100

    🚀 Model

--- src/shared/Level/models.ts
+++ src/shared/Level/models.ts
@@ -1,9 +1,12 @@
+import type { ImageProps } from '@shared/Image/models';
+
 export type LevelSize = 'small' | 'medium' | 'large';
 
 export type LevelRank = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export interface LevelProps {
   className?: string;
+  badgeSrc?: ImageProps['src'];
   currentXp?: number;
   rank?: LevelRank;
   showProgressBar?: boolean;
