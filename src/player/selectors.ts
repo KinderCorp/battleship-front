d437f6a332faf1c9a49a81e5bc68366a994efce3
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-12-08 15:14:14 +0100

    🚨 Fix lint

--- src/player/selectors.ts
+++ src/player/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-import type { GuestPlayer, PlayerState } from '@player/models';
+import type { GuestPlayer, LoggedPlayer, PlayerState } from '@player/models';
 import { PLAYER_NAME } from '@player/constants';
 import type { RootState } from '@core/models';
 
@@ -14,5 +14,5 @@ const selectPlayerState = (state: RootState): PlayerState => state[PLAYER_NAME];
 
 export const selectPlayer = createSelector(
   selectPlayerState,
-  (state: PlayerState): GuestPlayer => state.player,
+  (state: PlayerState): Partial<GuestPlayer | LoggedPlayer> => state.player,
 );
