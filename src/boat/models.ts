e7de2f9b40ddf1e1ebc5b7506fd3d006acbb206c
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-12-15 21:21:59 +0100

    🚚 Move component Boat in boat domain

--- src/boat/models.ts
+++ src/boat/models.ts
@@ -1,4 +1,19 @@
+import type { CSSProperties } from 'react';
+
 import type { ImageProps } from '@shared/Image/models';
+
+export type BoatDirection = 'east' | 'north' | 'south' | 'west';
+
+export interface BoatProps extends Pick<ImageProps, 'priority'>, Pick<Boat, 'name' | 'src'> {
+  className?: string;
+  direction?: BoatDirection;
+  height: number; // For the length
+  index: number;
+  style?: CSSProperties;
+  isPlacementActive?: boolean;
+  width: number;
+  onRotation?: (index: number) => boolean;
+}
 
 export interface Boat {
   lengthCell: number;
