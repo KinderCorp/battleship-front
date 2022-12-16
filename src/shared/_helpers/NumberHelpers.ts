83be35291290866a1a389e46e1e984bf45dd0300
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-12-16 15:33:22 +0100

    🚀 Add parseInt method in NumberHelpers

--- src/shared/_helpers/NumberHelpers.ts
+++ src/shared/_helpers/NumberHelpers.ts
@@ -1,3 +1,5 @@
+import type { IntRange } from '@core/models';
+
 class NumberHelpers {
   /**
    * Generate random number in given range.
@@ -8,6 +10,17 @@ class NumberHelpers {
    */
   static randomNumber(min: number, max: number): number {
     return Math.round(Math.random() * (max - min) + min);
+  }
+
+  /**
+   * Parse a number.
+   *
+   * @param {any} value Value to convert
+   * @param {number} radix Base of the number
+   * @return {number}
+   */
+  static parseInt(value: any, radix?: IntRange<2, 36>): number {
+    return parseInt(value, radix || 10);
   }
 }
 
