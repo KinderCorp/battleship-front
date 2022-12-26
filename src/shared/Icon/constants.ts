24268a429939d5c1ec957c3ec0db356001eee0cc
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-12-26 15:45:39 +0100

    🎨 Use variable for dynamic options

--- src/shared/Icon/constants.ts
+++ src/shared/Icon/constants.ts
@@ -1,28 +1,31 @@
 import type { ComponentType } from 'react';
 import dynamic from 'next/dynamic';
+import type { DynamicOptions } from 'next/dynamic';
 
 import type { IconName, IconSvgProps } from '@shared/Icon/models';
 
+const OPTIONS: DynamicOptions<IconSvgProps> = { suspense: false };
+
 export const ICON_LIST: Record<IconName, ComponentType<IconSvgProps>> = {
-  Add: dynamic(() => import('@shared/Icon/components/IconAdd'), { suspense: false }),
-  ArrowLeft: dynamic(() => import('@shared/Icon/components/IconArrowLeft'), { suspense: false }),
-  Boat: dynamic(() => import('@shared/Icon/components/IconBoat'), { suspense: false }),
-  Check: dynamic(() => import('@shared/Icon/components/IconCheck'), { suspense: false }),
-  Close: dynamic(() => import('@shared/Icon/components/IconClose'), { suspense: false }),
-  Copy: dynamic(() => import('@shared/Icon/components/IconCopy'), { suspense: false }),
-  Dice: dynamic(() => import('@shared/Icon/components/IconDice'), { suspense: false }),
-  Eye: dynamic(() => import('@shared/Icon/components/IconEye'), { suspense: false }),
-  EyeHide: dynamic(() => import('@shared/Icon/components/IconEyeHide'), { suspense: false }),
-  GearWheel: dynamic(() => import('@shared/Icon/components/IconGearWheel'), { suspense: false }),
-  Infinity: dynamic(() => import('@shared/Icon/components/IconInfinity'), { suspense: false }),
-  Loader: dynamic(() => import('@shared/Icon/components/IconLoader'), { suspense: false }),
-  Lock: dynamic(() => import('@shared/Icon/components/IconLock'), { suspense: false }),
-  Logout: dynamic(() => import('@shared/Icon/components/IconLogout'), { suspense: false }),
-  Pen: dynamic(() => import('@shared/Icon/components/IconPen'), { suspense: false }),
-  People: dynamic(() => import('@shared/Icon/components/IconPeople'), { suspense: false }),
-  Reload: dynamic(() => import('@shared/Icon/components/IconReload'), { suspense: false }),
-  Remove: dynamic(() => import('@shared/Icon/components/IconRemove'), { suspense: false }),
-  Share: dynamic(() => import('@shared/Icon/components/IconShare'), { suspense: false }),
-  Target: dynamic(() => import('@shared/Icon/components/IconTarget'), { suspense: false }),
-  Trophy: dynamic(() => import('@shared/Icon/components/IconTrophy'), { suspense: false }),
+  Add: dynamic(() => import('@shared/Icon/components/IconAdd'), OPTIONS),
+  ArrowLeft: dynamic(() => import('@shared/Icon/components/IconArrowLeft'), OPTIONS),
+  Boat: dynamic(() => import('@shared/Icon/components/IconBoat'), OPTIONS),
+  Check: dynamic(() => import('@shared/Icon/components/IconCheck'), OPTIONS),
+  Close: dynamic(() => import('@shared/Icon/components/IconClose'), OPTIONS),
+  Copy: dynamic(() => import('@shared/Icon/components/IconCopy'), OPTIONS),
+  Dice: dynamic(() => import('@shared/Icon/components/IconDice'), OPTIONS),
+  Eye: dynamic(() => import('@shared/Icon/components/IconEye'), OPTIONS),
+  EyeHide: dynamic(() => import('@shared/Icon/components/IconEyeHide'), OPTIONS),
+  GearWheel: dynamic(() => import('@shared/Icon/components/IconGearWheel'), OPTIONS),
+  Infinity: dynamic(() => import('@shared/Icon/components/IconInfinity'), OPTIONS),
+  Loader: dynamic(() => import('@shared/Icon/components/IconLoader'), OPTIONS),
+  Lock: dynamic(() => import('@shared/Icon/components/IconLock'), OPTIONS),
+  Logout: dynamic(() => import('@shared/Icon/components/IconLogout'), OPTIONS),
+  Pen: dynamic(() => import('@shared/Icon/components/IconPen'), OPTIONS),
+  People: dynamic(() => import('@shared/Icon/components/IconPeople'), OPTIONS),
+  Reload: dynamic(() => import('@shared/Icon/components/IconReload'), OPTIONS),
+  Remove: dynamic(() => import('@shared/Icon/components/IconRemove'), OPTIONS),
+  Share: dynamic(() => import('@shared/Icon/components/IconShare'), OPTIONS),
+  Target: dynamic(() => import('@shared/Icon/components/IconTarget'), OPTIONS),
+  Trophy: dynamic(() => import('@shared/Icon/components/IconTrophy'), OPTIONS),
 };
