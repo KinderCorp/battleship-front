dbff388ce20b3b6b5a463a489a29688704134ad3
DrakenLine <43884146+DrakenLine@users.noreply.github.com>
2022-12-30 17:39:55 +0100

    Component Item (#30)
    * 🚀 Model
    
    * 🚀 Init Item component
    
    * 🎨 fix reviews
    
    * 💄 clean styles
    
    * 💄 update css
    
    * 🎨 improve code
    
    * 🎨 formatting
    
    * 🎨 Return type value
    
    * 🚚 Move component in weapon domain
    
    * 💄 Mixin light-reflection-round
    
    * 💄 Fix import WeaponItem
    
    * 🚀 NumberHelpers isNumber
    
    * 🚀 WeaponItem component
    
    * 🏷️ Use weapon model
    
    * 🚨 Fix parser weapon
    
    * 💬 Better alt image for weapon item
    
    * 💫 Animations styles
    
    Co-authored-by: Alexandra Farina <alexandra.farina@mmibordeaux.com>
    Co-authored-by: Quentin Masbernat <masbernat.quentin@gmail.com>

---
+++ src/weapon/components/WeaponItem.tsx
@@ -0,0 +1,98 @@
+import { useCallback, useMemo } from 'react';
+import classNames from 'clsx';
+import type { SyntheticEvent } from 'react';
+
+import { COLORS } from '@core/constants';
+import Icon from '@shared/Icon/components/Icon';
+import Image from '@shared/Image/components/Image';
+import NumberHelpers from '@helpers/NumberHelpers';
+import useTranslation from '@hooks/useTranslation';
+import type { WeaponItemProps } from '@weapon/models';
+
+/**
+ * WeaponItem component.
+ *
+ * @param {WeaponItemProps} props Props
+ * @return {JSX.Element}
+ */
+const WeaponItem = ({
+  className = '',
+  counter,
+  isLocked = false,
+  isSelected = false,
+  onClick,
+  weapon,
+}: WeaponItemProps): JSX.Element => {
+  const { translate } = useTranslation();
+
+  const { name, src } = weapon;
+
+  const checkIsDisabled = useCallback(() => {
+    if (isLocked) return true;
+
+    if (NumberHelpers.isNumber(counter) && (counter < -1 || counter === 0)) return true;
+
+    return false;
+  }, [counter, isLocked]);
+
+  const counterActive = useMemo(() => NumberHelpers.isNumber(counter) && counter >= -1, [counter]);
+  const disabled = useMemo(() => checkIsDisabled(), [checkIsDisabled]);
+
+  const handleClick = useCallback(
+    (event: SyntheticEvent<EventTarget>) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (!disabled) onClick(name);
+    },
+    [disabled, name, onClick],
+  );
+
+  const itemClassName = useMemo(
+    (): string =>
+      classNames(
+        'weapon-item',
+        {
+          'weapon-item--has-counter': !!counterActive && !isLocked,
+          'weapon-item--is-disabled': !!disabled,
+          'weapon-item--is-locked': !!isLocked,
+          'weapon-item--is-selected': !!isSelected,
+        },
+        className,
+      ),
+    [className, counterActive, disabled, isLocked, isSelected],
+  );
+
+  return (
+    <div className={itemClassName} onClick={handleClick}>
+      <Image
+        alt={translate('weapon', { name })}
+        className="weapon-item-image"
+        objectFit="cover"
+        sizes={{ default: '100px' }}
+        src={src}
+      />
+
+      {!!isLocked && (
+        <Icon
+          borderColor={COLORS.BLACK}
+          className="weapon-item-lock"
+          color={COLORS.WHITE}
+          name="Lock"
+        />
+      )}
+
+      {!!counterActive && !isLocked && (
+        <div className="weapon-item-counter">
+          {counter === -1 ? (
+            <Icon name="Infinity" color={COLORS.BROWN} borderColor={COLORS.ORANGE} size="small" />
+          ) : (
+            counter
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeaponItem;
--- src/weapon/models.ts
+++ src/weapon/models.ts
@@ -1,8 +1,19 @@
+import type { ImageProps } from '@shared/Image/models';
 import type { Position } from '@shared/Board/models';
 
 export interface Weapon {
-  id: number;
-  name: string;
-  maxAmmunition: number;
   damageArea: Position[];
+  id: number;
+  maxAmmunition: number;
+  name: string;
+  src: ImageProps['src'];
+}
+
+export interface WeaponItemProps {
+  className?: string;
+  counter?: number;
+  isLocked?: boolean;
+  isSelected?: boolean;
+  weapon: Weapon;
+  onClick: (name: Weapon['name']) => void;
 }
