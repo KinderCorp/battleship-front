12108d19ca2657e297d4238754e806d111a683a3
Quentin Masbernat <masbernat.quentin@gmail.com>
2023-01-05 19:36:21 +0100

    🚀 Don't show WeaponList if weaponItems is empty

--- src/weapon/components/WeaponList.tsx
+++ src/weapon/components/WeaponList.tsx
@@ -1,6 +1,7 @@
 import classNames from 'clsx';
 import { useMemo } from 'react';
 
+import ArrayHelpers from '@helpers/ArrayHelpers';
 import WeaponItem from '@weapon/components/WeaponItem';
 import type { WeaponListProps } from '@weapon/models';
 
@@ -8,19 +9,21 @@ import type { WeaponListProps } from '@weapon/models';
  * WeaponList component.
  *
  * @param {WeaponListProps} props Props
- * @return {JSX.Element}
+ * @return {JSX.Element|null}
  */
 const WeaponList = ({
   className = '',
   isDisabled = false,
   onClick,
   weaponItems,
-}: WeaponListProps): JSX.Element => {
+}: WeaponListProps): JSX.Element | null => {
   const weaponListClassName = useMemo(
     (): string =>
       classNames('weapon-list', { 'weapon-list--is-disabled': !!isDisabled }, className),
     [className, isDisabled],
   );
+
+  if (ArrayHelpers.isEmpty(weaponItems)) return null;
 
   return (
     <div className={weaponListClassName}>
